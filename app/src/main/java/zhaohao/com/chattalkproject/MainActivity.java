ba29166fb62456763deea63b731f2dae65c2bc8f
zhaohao0821 <405869257@qq.com>
2018-03-28 22:02:02 +0800

    修改

--- app/src/main/java/zhaohao/com/chattalkproject/MainActivity.java
+++ app/src/main/java/zhaohao/com/chattalkproject/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //设置布局
         setContentView(R.layout.activity_main);
     }
 }
